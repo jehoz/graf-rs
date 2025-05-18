69f26a5f56cedbc8a31242169bbf317f7a51ab9d
Jesse Holzman <jesseholzman96@gmail.com>
2025-05-18 14:43:59 -0400

    Open context menu on right click

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
-use macroquad::prelude::*;
+use macroquad::{
+    prelude::*,
+    ui::{hash, root_ui},
+};
 
 #[derive(Eq, Hash, PartialEq)]
 struct NodeId(u32);
@@ -31,12 +34,20 @@ const HALF_NODE_SIZE: f32 = NODE_SIZE / 2.0;
 async fn main() {
     let mut node_id_counter: u32 = 0;
     let mut nodes: HashMap<NodeId, CircuitNode> = HashMap::new();
-    // let mut cursor:
+
+    let mut context_menu_position: Option<Vec2> = None;
 
     loop {
         clear_background(BLACK);
 
-        draw_text("G R A f", 20.0, 20.0, 30.0, WHITE);
+        let (mouse_x, mouse_y) = mouse_position();
+        draw_text(
+            &format!("Mouse: ({mouse_x}, {mouse_y})"),
+            20.0,
+            20.0,
+            30.0,
+            WHITE,
+        );
 
         for (_node_id, node) in nodes.iter() {
             draw_rectangle_lines(
@@ -50,14 +61,29 @@ async fn main() {
         }
 
         if is_mouse_button_released(MouseButton::Right) {
-            let (nx, ny) = mouse_position();
-            nodes.insert(
-                NodeId(node_id_counter),
-                CircuitNode {
-                    pos: Vec2::new(nx, ny),
-                },
-            );
-            node_id_counter += 1;
+            let (mx, my) = mouse_position();
+            context_menu_position = Some(vec2(mx, my));
+            // nodes.insert(
+            //     NodeId(node_id_counter),
+            //     CircuitNode {
+            //         pos: Vec2::new(nx, ny),
+            //     },
+            // );
+            // node_id_counter += 1;
+        }
+
+        if let Some(pos) = context_menu_position {
+            root_ui().window(hash!(), pos, vec2(50., 90.), |ui| {
+                if ui.button(None, "Clock") {
+                    context_menu_position = None;
+                }
+                if ui.button(None, "Gate") {
+                    context_menu_position = None;
+                }
+                if ui.button(None, "Note") {
+                    context_menu_position = None;
+                }
+            });
         }
 
         next_frame().await
