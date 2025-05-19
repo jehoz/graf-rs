c27f64540476267ff5e7b173f0e3d9b96d3c7631
Jesse Holzman <jesseholzman96@gmail.com>
2025-05-19 10:55:37 -0400

    Add default implementations for device types

--- src/devices.rs
+++ src/devices.rs
@@ -10,14 +10,45 @@ pub struct ClockDevice {
     offset: f32,
 }
 
+impl Default for ClockDevice {
+    fn default() -> Self {
+        ClockDevice {
+            frequency: Frequency::Beats(BeatFraction {
+                numerator: 1,
+                denominator: 4,
+            }),
+            duty_cycle: 0.5,
+            offset: 0.,
+        }
+    }
+}
+
 pub struct GateDevice {
     operation: BooleanOperation,
+}
+
+impl Default for GateDevice {
+    fn default() -> Self {
+        GateDevice {
+            operation: BooleanOperation::AND,
+        }
+    }
 }
 
 pub struct NoteDevice {
     midi_note: u8,
     velocity: u8,
     is_on: bool,
+}
+
+impl Default for NoteDevice {
+    fn default() -> Self {
+        NoteDevice {
+            midi_note: 60,
+            velocity: 100,
+            is_on: false,
+        }
+    }
 }
 
 pub enum BooleanOperation {
