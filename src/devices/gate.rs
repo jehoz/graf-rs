a8964f5c41bd9c949e6024837cbc796082eeeb5c
holzmanj <jesseholzman96@gmail.com>
2025-06-10 09:07:10 -0400

    Draw operator symbol on gate

--- src/devices/gate.rs
+++ src/devices/gate.rs
@@ -1,7 +1,8 @@
 use macroquad::{
-    color::WHITE,
+    color::{BLACK, WHITE},
     math::{vec2, Vec2},
-    shapes::draw_rectangle_lines,
+    shapes::{draw_circle, draw_line, draw_rectangle, draw_rectangle_lines},
+    text::draw_text,
 };
 
 use super::{Arity, Device, GATE_WIDTH};
@@ -68,6 +69,13 @@ impl Device for Gate {
 
     fn draw(&self) {
         let Vec2 { x, y } = self.position;
+        draw_rectangle(
+            x - GATE_WIDTH / 2.,
+            y - GATE_WIDTH / 2.,
+            GATE_WIDTH,
+            GATE_WIDTH,
+            BLACK,
+        );
         draw_rectangle_lines(
             x - GATE_WIDTH / 2.,
             y - GATE_WIDTH / 2.,
@@ -76,6 +84,8 @@ impl Device for Gate {
             2.0,
             WHITE,
         );
+
+        draw_AND(x, y, GATE_WIDTH * 0.5);
     }
 
     fn input_arity(&self) -> Arity {
@@ -85,4 +95,18 @@ impl Device for Gate {
     fn has_output(&self) -> bool {
         true
     }
+}
+
+fn draw_AND(x: f32, y: f32, scale: f32) {
+    let top = y - scale / 2.0;
+    let bottom = y + scale / 2.0;
+    let left = x - scale / 2.0;
+    let right = x + scale / 2.0;
+
+    draw_line(left, bottom, x, top, 1.0, WHITE);
+    draw_line(x, top, right, bottom, 1.0, WHITE);
+
+    draw_circle(left, bottom, 2.0, WHITE);
+    draw_circle(right, bottom, 2.0, WHITE);
+    draw_circle(x, top, 2.0, WHITE);
 }
