303e4d17a8bad76caa4bf7023e252af6d8cd3f4c
holzmanj <jesseholzman96@gmail.com>
2025-09-16 09:55:34 -0400

    Draw 12 rectangles representing keyboard notes (not clickable yet)

--- src/widgets/note_picker.rs
+++ src/widgets/note_picker.rs
@@ -24,21 +24,73 @@ impl<'a> Widget for NotePicker<'a> {
         let desired_size = Vec2::new(self.width, self.width * 0.5);
         let mut response = ui.allocate_response(desired_size, Sense::click());
 
-        let key_width = response.rect.width() / 8.0;
+        let key_width = response.rect.width() / 7.0;
         if ui.is_rect_visible(response.rect) {
             // white keys
-            for i in 0..=7 {
-                let key_rect = Rect::from_min_max(
-                    Pos2::new(i as f32 * key_width, 0.0),
-                    Pos2::new((i + 1) as f32 * key_width, response.rect.height()),
+            for i in 0..7 {
+                let key_rect = Rect::from_center_size(
+                    Pos2::new(
+                        response.rect.left() + (i as f32 + 0.5) * key_width,
+                        response.rect.center().y,
+                    ),
+                    Vec2::new(key_width, response.rect.height()),
                 );
-                let key = Shape::rect_stroke(
+
+                ui.painter().add(Shape::rect_filled(
                     key_rect,
                     self.key_corner_radius,
-                    Stroke::new(1.0, ui.visuals().text_color()),
-                    StrokeKind::Outside,
+                    ui.visuals().weak_text_color(),
+                ));
+                ui.painter().add(Shape::rect_stroke(
+                    key_rect,
+                    self.key_corner_radius,
+                    Stroke::new(1.0, ui.visuals().extreme_bg_color),
+                    StrokeKind::Inside,
+                ));
+            }
+
+            // black keys
+            for i in 0..2 {
+                let key_rect = Rect::from_center_size(
+                    Pos2::new(
+                        response.rect.left() + (i as f32 + 1.0) * key_width,
+                        response.rect.center().y - (response.rect.height() * 0.25 * 0.67),
+                    ),
+                    Vec2::new(key_width * 0.67, response.rect.height() * 0.67),
                 );
-                ui.painter().add(key);
+
+                ui.painter().add(Shape::rect_filled(
+                    key_rect,
+                    self.key_corner_radius,
+                    ui.visuals().weak_text_color(),
+                ));
+                ui.painter().add(Shape::rect_stroke(
+                    key_rect,
+                    self.key_corner_radius,
+                    Stroke::new(2.0, ui.visuals().extreme_bg_color),
+                    StrokeKind::Inside,
+                ));
+            }
+            for i in 0..3 {
+                let key_rect = Rect::from_center_size(
+                    Pos2::new(
+                        response.rect.left() + (i as f32 + 4.0) * key_width,
+                        response.rect.center().y - (response.rect.height() * 0.25 * 0.67),
+                    ),
+                    Vec2::new(key_width * 0.67, response.rect.height() * 0.67),
+                );
+
+                ui.painter().add(Shape::rect_filled(
+                    key_rect,
+                    self.key_corner_radius,
+                    ui.visuals().weak_text_color(),
+                ));
+                ui.painter().add(Shape::rect_stroke(
+                    key_rect,
+                    self.key_corner_radius,
+                    Stroke::new(2.0, ui.visuals().extreme_bg_color),
+                    StrokeKind::Inside,
+                ));
             }
         }
 
